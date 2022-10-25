6febac6eb2b65e0332ee1ae250d5e48d910e3175
preston <prestonprince18@gmail.com>
2022-10-25 17:08:20 -0600

    Add get all spots endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,9 +1,40 @@
 const express = require('express');
 
+const { Spot } = require('../../db/models');
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.json({message: 'u made it!'})
+router.get('/', async (req, res, next) => {
+    const spots = await Spot.findAll();
+    // get avgrating and previewimage for each spot
+    for (const spot of spots) {
+        const spotData = spot.dataValues;
+
+        // get reviews for current spot, returns array of reviews
+        const allReviews = await spot.getReviews();
+        
+        // get each review for current spot
+        let sumStars = 0;
+        for (const review of allReviews) {
+            const stars = review.dataValues.stars;
+            sumStars += stars;
+        };
+
+        const avgRating = sumStars / allReviews.length;
+        spotData.avgRating = avgRating;
+
+        // array of all images that are preview for current spot
+        const allImages = await spot.getSpotImages({where: {preview: true}});
+        for (const image of allImages) {
+            const url = image.dataValues.url;
+            spotData.previewImage = url
+        }
+    };
+
+    const resObj = {};
+    resObj.Spots = spots;
+
+    res.json(resObj);
 })
 
 module.exports = router;
