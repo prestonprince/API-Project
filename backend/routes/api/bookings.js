830c7af19b84245e60e1d73debca1643d415b4b5
preston <prestonprince18@gmail.com>
2022-10-27 01:00:47 -0600

    add endpoint to edit booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -7,13 +7,14 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+
 // get current users bookings
 router.get('/current', requireAuth, async (req, res, next) => {
     const { user } = req;
     const bookings = await Booking.findAll({ where: {userId: user.id} });
     const bookingsArr = await Promise.all(bookings.map(async book => {
         const bookData = book.dataValues;
-
+        
         const spot = await Spot.findOne({
             where: {id: bookData.spotId},
             attributes: {
@@ -29,14 +30,42 @@ router.get('/current', requireAuth, async (req, res, next) => {
         });
         
         spot.dataValues.previewImage = prevImg.url;
-
+        
         bookData.Spot = spot.dataValues
-
+        
         return bookData
     }));
-
+    
     const resObj= { Bookings: bookingsArr };
     res.json(resObj);
 });
+
+// edit booking
+router.put('/:bookingId', requireAuth, async (req, res, next) => {
+    const id = req.params.bookingId;
+    const booking = await Booking.findByPk(id);
+    const { startDate, endDate } = req.body;
+
+    if (!booking) {
+        return res.status(404).json({
+            message: "Booking couldn't be found",
+            statusCode: 404
+        });
+    };
+
+    if (startDate > endDate) {
+        return res.status(400).json({
+            message: "Validation error",
+            statusCode: 400,
+            errors: {
+                endDate: "endDate cannot come before startDate"
+            }
+        });
+    };
+
+    booking.update({startDate, endDate});
+
+    res.json(booking)
+})
 
 module.exports = router;
