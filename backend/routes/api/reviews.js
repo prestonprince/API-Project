0255758ae2bf0f417eb4ff5f54969d93fc06bb0b
preston <prestonprince18@gmail.com>
2022-10-26 21:56:15 -0600

    add endpoint to create image for review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,14 +1,47 @@
 const express = require('express');
 
-const { Spot, SpotImage, Review, User } = require('../../db/models');
+const { Spot, SpotImage, Review, User, ReviewImage } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 
 const router = express.Router();
+
+// create image for review
+router.post('/:reviewId/images', requireAuth, async (req, res, next) => {
+    const { reviewId } = req.params;
+    const { url } = req.body;
+
+    const review = await Review.findByPk(reviewId);
+
+    if (!review) {
+        res.status(404).json({
+            message: "Review couldn't be found",
+            statusCode: 404
+        });
+    };
+
+    const revImgs = await review.getReviewImages();
+
+    if (revImgs.length >= 10) {
+        res.status(403).json({
+            message: "Maximum number of images for this resource was reached",
+            statusCode: 403
+        });
+    }
+
+    const newImg = await ReviewImage.create({ reviewId, url });
+    const img = await ReviewImage.findOne({
+        where: { id: newImg.id }, 
+        attributes: { exclude: ['reviewId', 'createdAt', 'updatedAt'] }
+    });
+
+    res.json(img);
+})
 
 // get reviews of current user
 router.get('/current', requireAuth, async (req, res, next) => {
     const { user } = req;
     const reviews = await user.getReviews();
+    if (!reviews.length) {res.json({message: "No reviews for this user"})}
 
     const reviewArr = await Promise.all(reviews.map(async rev => {
         const revData = rev.dataValues;
