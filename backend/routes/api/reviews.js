0e4b7d13f274af08dba5db2c4953f11f313523a7
preston <prestonprince18@gmail.com>
2022-10-26 23:03:51 -0600

    endpoint to edit review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const { Spot, SpotImage, Review, User, ReviewImage } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
+
+const validateReview = [
+    check('review')
+        .exists({ checkFalsy: true })
+        .withMessage("Review text is required"),
+    check('stars')
+        .exists({ checkFalsy: true })
+        .withMessage("Stars are required")
+        .isInt()
+        .isIn([1, 2, 3, 4, 5])
+        .withMessage("Stars must be an integer from 1 to 5"),
+        handleValidationErrors
+];
 
 // create image for review
 router.post('/:reviewId/images', requireAuth, async (req, res, next) => {
@@ -35,7 +50,7 @@ router.post('/:reviewId/images', requireAuth, async (req, res, next) => {
     });
 
     res.json(img);
-})
+});
 
 // get reviews of current user
 router.get('/current', requireAuth, async (req, res, next) => {
@@ -89,5 +104,24 @@ router.get('/current', requireAuth, async (req, res, next) => {
     const resObj = { Reviews: reviewArr };
     res.json(resObj);
 });
+
+
+// edit review
+router.put('/:reviewId', validateReview, async (req, res, next) => {
+    const id = req.params.reviewId;
+    const rev = await Review.findByPk(id);
+    const { review, stars } = req.body;
+
+    if (!rev) {
+        return res.status(404).json({
+            message: "Review couldn't be found",
+            statusCode: 404
+        });
+    };
+
+    rev.update({review, stars});
+
+    res.json(rev)
+})
 
 module.exports = router;
