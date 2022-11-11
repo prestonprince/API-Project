be0560a601ff6379307ce518b99aaf4c7aa7d969
preston <prestonprince18@gmail.com>
2022-11-10 17:12:01 -0700

    add restore user functionality to frontend

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -36,6 +36,13 @@ export const login = (user) => async (dispatch) => {
 
 };
 
+export const restoreUser = () => async(dispatch) => {
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data));
+  return response;
+}
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
