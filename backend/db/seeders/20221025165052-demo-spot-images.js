0c7f338e39198f84cd26ed6c3da9914161407ad9
preston <prestonprince18@gmail.com>
2022-11-17 07:50:53 -0700

    updated seed data

--- backend/db/seeders/20221025165052-demo-spot-images.js
+++ backend/db/seeders/20221025165052-demo-spot-images.js
@@ -6,52 +6,252 @@ module.exports = {
     return queryInterface.bulkInsert('SpotImages', [
       {
         spotId: 1,
-        url: 'https://www.fancypantshomes.com/wp-content/uploads/2020/03/original_1333066725walts.jpg',
+        url: 'https://ap.rdcpix.com/bc871471ae3498803b907cb5caeeba24l-b1705753047od-w480_h360_x2_var-q2.webp',
         preview: true
       },
       {
         spotId: 1,
-        url: 'https://freerangestock.com/sample/28816/breaking-bad-walter-white-house.jpg',
+        url: 'https://ap.rdcpix.com/bc871471ae3498803b907cb5caeeba24l-b3713483009od-w1024_h768_x2_var-q2.webp',
+        preview: false
+      },
+      {
+        spotId: 1,
+        url: 'https://ap.rdcpix.com/bc871471ae3498803b907cb5caeeba24l-b1639156203od-w1024_h768_x2_var-q2.webp',
+        preview: false
+      },
+      {
+        spotId: 1,
+        url: 'https://ap.rdcpix.com/bc871471ae3498803b907cb5caeeba24l-b1096083400od-w1024_h768_x2_var-q2.webp',
+        preview: false
+      },
+      {
+        spotId: 1,
+        url: 'https://ap.rdcpix.com/bc871471ae3498803b907cb5caeeba24l-b661509071od-w1024_h768_x2_var-q2.webp',
         preview: false
       },
       {
         spotId: 2,
-        url: 'https://upload.wikimedia.org/wikipedia/commons/e/ee/Image_Parkway_Gardens.jpg',
+        url: 'https://ap.rdcpix.com/ccefd324620f4e6e6129a82d7243e952l-m3474610090od-w480_h360_x2.webp',
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: 'https://ap.rdcpix.com/ccefd324620f4e6e6129a82d7243e952l-m1765264110od-w1024_h768_x2.webp',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://ap.rdcpix.com/ccefd324620f4e6e6129a82d7243e952l-m1743568859od-w1024_h768_x2.webp',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://ap.rdcpix.com/ccefd324620f4e6e6129a82d7243e952l-m1078089782od-w1024_h768_x2.webp',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://ap.rdcpix.com/ccefd324620f4e6e6129a82d7243e952l-m896715117od-w1024_h768_x2.webp',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://ap.rdcpix.com/c860342fa7c3ddbf4c3ea36d652d2464l-m2236096908od-w480_h360_x2.webp',
         preview: true
       },
       {
         spotId: 3,
-        url: 'https://i.pinimg.com/736x/d7/3e/55/d73e55ab547743fb3658eb8bd9b46d8c--regular-show.jpg',
+        url: 'https://ap.rdcpix.com/c860342fa7c3ddbf4c3ea36d652d2464l-m1131757314od-w1024_h768_x2.webp',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://ap.rdcpix.com/c860342fa7c3ddbf4c3ea36d652d2464l-m2613382416od-w1024_h768_x2.webp',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://ap.rdcpix.com/c860342fa7c3ddbf4c3ea36d652d2464l-m1934694113od-w1024_h768_x2.webp',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://ap.rdcpix.com/c860342fa7c3ddbf4c3ea36d652d2464l-m3693549895od-w1024_h768_x2.webp',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://ap.rdcpix.com/6a5251d54ed1f41245af4e4d62f1273dl-m1727032219od-w1024_h768_x2_var-q2.webp',
         preview: true
       },
       {
         spotId: 4,
-        url: 'https://m.media-amazon.com/images/M/MV5BYjFkNjU3ZGEtNTBjOS00ZTYyLTkxZGQtMDJkYjQ3M2I2ODk5XkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg',
+        url: 'https://ap.rdcpix.com/6a5251d54ed1f41245af4e4d62f1273dl-m1965621103od-w1024_h768_x2_var-q2.webp',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://ap.rdcpix.com/6a5251d54ed1f41245af4e4d62f1273dl-m4099723643od-w1024_h768_x2_var-q2.webp',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://ap.rdcpix.com/6a5251d54ed1f41245af4e4d62f1273dl-m2269531987od-w1024_h768_x2_var-q2.webp',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://ap.rdcpix.com/6a5251d54ed1f41245af4e4d62f1273dl-m178157747od-w1024_h768_x2_var-q2.webp',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://ap.rdcpix.com/af244054ba34655fdcb803c3c1e0659al-m1690082458od-w480_h360_x2.webp',
         preview: true
       },
       {
         spotId: 5,
-        url: 'https://media.istockphoto.com/id/1141078602/photo/emerald-lake-on-a-cloudy-day-with-its-thawed-lake-summer-and-fun-rocky-mountain-canada.jpg?b=1&s=170667a&w=0&k=20&c=0hv6eWuKuPhYAHdKo4rJPgrz-aW-bUBCn7-JSOGBvX8=',
+        url: "https://ap.rdcpix.com/af244054ba34655fdcb803c3c1e0659al-m1602648609od-w1024_h768_x2.webp",
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: "https://ap.rdcpix.com/af244054ba34655fdcb803c3c1e0659al-m2343413259od-w1024_h768_x2.webp",
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: "https://ap.rdcpix.com/af244054ba34655fdcb803c3c1e0659al-m1544354130od-w1024_h768_x2.webp",
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: "https://ap.rdcpix.com/af244054ba34655fdcb803c3c1e0659al-m2891034621od-w1024_h768_x2.webp",
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: "https://ar.rdcpix.com/244db69fea14e7b4b739e1939e5c4ddfc-f3126485525od-w1024_h768_x2.webp",
         preview: true
       },
       {
-        spotId: 5,
-        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ_LMvf59pxaT_xTfQCg77Qyk8B0oNc6V4veBMqIMtOjA1koEKzUiCkN3o3wKphh7iHDU&usqp=CAU',
+        spotId: 6,
+        url: "https://ar.rdcpix.com/244db69fea14e7b4b739e1939e5c4ddfc-f1100078859od-w1024_h768_x2.webp",
         preview: false
       },
       {
-        spotId: 5,
-        url: 'https://www.sunset.com/wp-content/uploads/cozy-cabins-cathedral-mountain-lodge-bc-pr-0919-900x500.jpg',
+        spotId: 6,
+        url: "https://ar.rdcpix.com/244db69fea14e7b4b739e1939e5c4ddfc-f1868569630od-w1024_h768_x2.webp",
         preview: false
       },
       {
-        spotId: 5,
-        url: 'https://c4.wallpaperflare.com/wallpaper/771/768/18/canada-cabin-bridge-wallpaper-thumb.jpg',
+        spotId: 6,
+        url: "https://ar.rdcpix.com/244db69fea14e7b4b739e1939e5c4ddfc-f3404396525od-w1024_h768_x2.webp",
         preview: false
       },
       {
-        spotId: 5,
-        url: 'https://s1.1zoom.me/b4854/318/Canada_Parks_Lake_Houses_Bridges_Emerald_Lake_Fir_513462_1366x768.jpg',
+        spotId: 6,
+        url: "https://ar.rdcpix.com/244db69fea14e7b4b739e1939e5c4ddfc-f3140105323od-w1024_h768_x2.webp",
+        preview: false
+      },
+      {
+        spotId: 7,
+        url: "https://ap.rdcpix.com/9070fa044835a43e1228eeea57e94cedl-m252082521od-w480_h360_x2_var-q2.webp",
+        preview: true
+      },
+      {
+        spotId: 7,
+        url: "https://ap.rdcpix.com/9070fa044835a43e1228eeea57e94cedl-m1967052464od-w1024_h768_x2_var-q2.webp",
+        preview: false
+      },
+      {
+        spotId: 7,
+        url: "https://ap.rdcpix.com/9070fa044835a43e1228eeea57e94cedl-m640637087od-w1024_h768_x2_var-q2.webp",
+        preview: false
+      },
+      {
+        spotId: 7,
+        url: "https://ap.rdcpix.com/9070fa044835a43e1228eeea57e94cedl-m3767628389od-w1024_h768_x2_var-q2.webp",
+        preview: false
+      },
+      {
+        spotId: 7,
+        url: "https://ap.rdcpix.com/9070fa044835a43e1228eeea57e94cedl-m2689793908od-w1024_h768_x2_var-q2.webp",
+        preview: false
+      },
+      {
+        spotId: 8,
+        url: "https://ar.rdcpix.com/690cadfe436b3f5ef8fab8002f17b847c-f1400561074od-w480_h360_x2.webp",
+        preview: true
+      },
+      {
+        spotId: 8,
+        url: "https://ar.rdcpix.com/690cadfe436b3f5ef8fab8002f17b847c-f4289454571od-w1024_h768_x2.webp",
+        preview: false
+      },
+      {
+        spotId: 8,
+        url: "https://ar.rdcpix.com/690cadfe436b3f5ef8fab8002f17b847c-f155371751od-w1024_h768_x2.webp",
+        preview: false
+      },
+      {
+        spotId: 8,
+        url: "https://ar.rdcpix.com/690cadfe436b3f5ef8fab8002f17b847c-f339928275od-w1024_h768_x2.webp",
+        preview: false
+      },
+      {
+        spotId: 8,
+        url: "https://ar.rdcpix.com/690cadfe436b3f5ef8fab8002f17b847c-f1749571333od-w1024_h768_x2.webp",
+        preview: false
+      },
+      {
+        spotId: 9,
+        url: "https://ap.rdcpix.com/fb5c631a6f656a36ecbbb64b72b311e6l-m483904480od-w480_h360_x2_var-q2.webp",
+        preview: true
+      },
+      {
+        spotId: 9,
+        url: "https://ap.rdcpix.com/fb5c631a6f656a36ecbbb64b72b311e6l-m2722132729od-w1024_h768_x2_var-q2.webp",
+        preview: false
+      },
+      {
+        spotId: 9,
+        url: "https://ap.rdcpix.com/fb5c631a6f656a36ecbbb64b72b311e6l-m3110033986od-w1024_h768_x2_var-q2.webp",
+        preview: false
+      },
+      {
+        spotId: 9,
+        url: "https://ap.rdcpix.com/fb5c631a6f656a36ecbbb64b72b311e6l-m2403852128od-w1024_h768_x2_var-q2.webp",
+        preview: false
+      },
+      {
+        spotId: 9,
+        url: "https://ap.rdcpix.com/fb5c631a6f656a36ecbbb64b72b311e6l-m1695496768od-w1024_h768_x2_var-q2.webp",
+        preview: false
+      },
+      {
+        spotId: 10,
+        url: "https://ap.rdcpix.com/5845370f9d9bee6f59178771ef691575l-m3050629480od-w480_h360_x2.webp",
+        preview: true
+      },
+      {
+        spotId: 10,
+        url: "https://ap.rdcpix.com/5845370f9d9bee6f59178771ef691575l-m729879494od-w1024_h768_x2.webp",
+        preview: false
+      },
+      {
+        spotId: 10,
+        url: "https://ap.rdcpix.com/5845370f9d9bee6f59178771ef691575l-m2959878120od-w1024_h768_x2.webp",
+        preview: false
+      },
+      {
+        spotId: 10,
+        url: "https://ap.rdcpix.com/5845370f9d9bee6f59178771ef691575l-m724719599od-w1024_h768_x2.webp",
+        preview: false
+      },
+      {
+        spotId: 10,
+        url: "https://ap.rdcpix.com/5845370f9d9bee6f59178771ef691575l-m1177784652od-w1024_h768_x2.webp",
         preview: false
       },
     ], {})
